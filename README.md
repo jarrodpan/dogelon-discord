f227a14ffb4918b285f060efe9062c7c16023430
Jarrod <jarrod.pan@gmail.com>
2022-02-26 17:56:50 +1100

    logs message

---
+++ README.md
@@ -0,0 +1,3 @@
+hurr durr
+
+https://stackabuse.com/guide-to-creating-a-discord-bot-in-javascript-with-discordjs-v13/
--- app.js
+++ app.js
@@ -1,2 +1,25 @@
-const discordjs = require("discord.js");
 
+require('dotenv').config();
+//console.log(process.env); // to test dotenv
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES] });
+
+// initialise
+client.once('ready', () => {
+	console.log('Ready!');
+
+	//	console.log(client.channels.cache);
+});
+
+
+// login to discord
+client.login(process.env.DISCORD_TOKEN);
+
+
+client.on('messageCreate', (message) => {
+	let server = client.channels.cache.get(message.channelId).guild.name;
+	let channel = client.channels.cache.get(message.channelId).name;
+
+	console.log("<" + server + "#" + channel + "@" + message.author.username + ">", message.content);
+});
