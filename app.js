43c06d9a63d6fe8b73f331ebf877abf1df33b26b
Jarrod <jarrod.pan@gmail.com>
2022-02-27 19:50:41 +1100

    recognise stuff

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+'use strict'
 
 require('dotenv').config();
 //console.log(process.env); // to test dotenv
 const { Client, Intents } = require('discord.js');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES] });
+
+const queue = [];
 
 // initialise
 client.once('ready', () => {
@@ -12,6 +15,9 @@ client.once('ready', () => {
 	//	console.log(client.channels.cache);
 });
 
+client.on('interactionCreate', interaction => {
+	console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
+});
 
 // login to discord
 client.login(process.env.DISCORD_TOKEN);
@@ -22,4 +28,31 @@ client.on('messageCreate', (message) => {
 	let channel = client.channels.cache.get(message.channelId).name;
 
 	console.log("<" + server + "#" + channel + "@" + message.author.username + ">", message.content);
+
+	// ignore self messages
+	if (message.author.bot) return;
+
+	// tokenize
+	let tokens = message.content.split(" ");
+	tokens.forEach(token => {
+		if (token.charAt(0) == "$") {
+			queue.push([message, token]);
+		}
+
+		if (token.slice(0, 2) == "r/" || token.slice(0, 3) == "/r/") queue.push([message, token]);
+
+	});
+
 });
+
+const parseLoop = setInterval(() => {
+	// skip if empty queue
+	if (queue.length == 0) return;
+
+	// get next item from queue
+	let [message, token] = queue.shift();
+
+	// send message to channel
+	return message.channel.send(token);
+
+}, 10);
