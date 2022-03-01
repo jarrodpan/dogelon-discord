59e6c36860cb24199a571d23418019c6d115e7b8
Jarrod <jarrod.pan@gmail.com>
2022-03-01 21:59:39 +1100

    define Command

--- src/types/Command.ts
+++ src/types/Command.ts
@@ -1,3 +1,12 @@
-export interface Command {
+import { Action } from "./Action";
 
+export const enum ExecuteOn {
+	TOKEN,
+	MESSAGE
+}
+
+export interface Command {
+	expression: string,
+	executeOn: ExecuteOn,
+	execute: (action: Action) => Promise<any>
 }
