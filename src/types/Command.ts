467a20213b4483518fdc37d3878363abd7b9ff9c
Jarrod <jarrod.pan@gmail.com>
2022-03-01 22:54:06 +1100

    command abstract class

--- src/types/Command.ts
+++ src/types/Command.ts
@@ -1,12 +1,19 @@
 import { Action } from "./Action";
 
-export const enum ExecuteOn {
+export const enum MatchOn {
 	TOKEN,
 	MESSAGE
 }
-
-export interface Command {
-	expression: string,
-	executeOn: ExecuteOn,
-	execute: (action: Action) => Promise<any>
+/**
+ * @author Jarrod Pan
+ * 
+ * Provides an abstract class to implement commands in Dogelon
+ * 
+ * @param expression: the regular expression as a string used to determine matching
+ * @param matchOn: a member of the @enum MatchOn to determine if 
+ */
+export abstract class Command {
+	public readonly expression!: string;
+	public readonly matchOn!: MatchOn;
+	public readonly execute!: (action: Action) => Promise<any> | any;
 }
