430cc067cae761b87865c4418c48e8bf5bc780db
Jarrod <jarrod.pan@gmail.com>
2022-03-01 22:54:25 +1100

    sample command (ask me what ligma is)

---
+++ src/commands/LigmaCommand.ts
@@ -0,0 +1,13 @@
+import { Action } from '../types/Action';
+import { Command, MatchOn } from '../types/Command'
+
+/**
+ * what's ligma?
+ */
+export class LigmaCommand implements Command {
+	expression = "what('{0,1}| i)s ligma\?*"; //"what's ligma";
+	matchOn = MatchOn.MESSAGE;
+	execute = (action: Action) => {
+		return "ligma balls";
+	}
+}
