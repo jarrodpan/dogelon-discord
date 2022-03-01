beab360d2877d53a52e5de8da7ca37216743cd37
Jarrod <jarrod.pan@gmail.com>
2022-03-01 22:58:42 +1100

    reddit command

---
+++ src/commands/RedditCommand.ts
@@ -0,0 +1,13 @@
+import { Action } from '../types/Action';
+import { Command, MatchOn } from '../types/Command'
+
+/**
+ * a command to turn r/ and /r/ references and reply with the link (as discord doesnt do it automatically for some reason)
+ */
+export class RedditCommand implements Command {
+	expression = "(r/|/r/)";
+	matchOn = MatchOn.TOKEN;
+	execute = (action: Action) => {
+		return "https://www.reddit.com" + (action.token.slice(0, 1) == "/" ? "" : "/") + action.token;
+	}
+}
