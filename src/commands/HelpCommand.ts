a6cb6077be6ec484d427491ff7603812fa4cc03f
Jarrod <jarrod.pan@gmail.com>
2022-03-06 02:09:48 +1100

    words

--- src/commands/HelpCommand.ts
+++ src/commands/HelpCommand.ts
@@ -18,7 +18,7 @@ export default class HelpCommand implements Command {
 			.setTitle(`🚀 Dogelon ${v}`)
 			.setThumbnail("https://i.imgur.com/2vHF2jl.jpg")
 			.setDescription("The not-so-stupid discord bot made for no reason. Written in Node.js and TypeScript.")
-			.addField("`${stock ticker}` (inline)", "Gets current price for a stock. Cached and/or delayed depending on API response and exchange opening times.")
+			.addField("`${stock ticker}` (inline)", "Gets current price for a stock. Cached and/or delayed depending on API response and exchange opening times. Yahoo tickers, add `.AX` for Australian stocks.")
 			.addField("`/r/{subreddit}`, `r/{subreddit}` (inline)", "Converts a subreddit reference to a reddit link, regardless of if it exists or not.")
 			.addField("`what's ligma?` (and some variants, inline)", "dare you to ask me")
 			.addField("`!help`, `!h`", "Displays this message")
