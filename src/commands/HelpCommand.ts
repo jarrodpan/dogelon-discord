b6a35e9ee86aa13d9d0dde16d06bd852712814bc
Jarrod <jarrod.pan@gmail.com>
2022-03-07 18:26:27 +1100

    updated help with crypto command

--- src/commands/HelpCommand.ts
+++ src/commands/HelpCommand.ts
@@ -17,6 +17,7 @@ export default class HelpCommand implements Command {
 			.setThumbnail("https://i.imgur.com/2vHF2jl.jpg")
 			.setDescription("The not-so-stupid discord bot made for no reason. Written in Node.js and TypeScript.")
 			.addField("`${stock ticker}` (inline)", "Gets current price for a stock. Cached and/or delayed depending on API response and exchange opening times. Yahoo tickers, add `.AX` for Australian stocks.")
+			.addField("`%{crypto ticker}` (inline)", "Gets current price for a cryptocurrency. Cached and/or delayed. Sourced from [CoinGecko](https://www.coingecko.com).")
 			.addField("`/r/{subreddit}`, `r/{subreddit}` (inline)", "Converts a subreddit reference to a reddit link, regardless of if it exists or not.")
 			.addField("`what's ligma?` (and some variants, inline)", "dare you to ask me")
 			.addField("`!help`, `!h`", "Displays this message")
