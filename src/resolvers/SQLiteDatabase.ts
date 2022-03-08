e7e6f9d86f49e509c6ef3723c82ba4fa4684e75a
Jarrod <jarrod.pan@gmail.com>
2022-03-09 01:33:49 +1100

    create table and use better-sqlite3

--- src/resolvers/SQLiteDatabase.ts
+++ src/resolvers/SQLiteDatabase.ts
@@ -1,8 +1,14 @@
 import Database from "../types/Database";
+import SQLiteDb from 'better-sqlite3'; // alias of 'Database'
 
 export default class SQLiteDatabase implements Database {
-	connect(uname: string, pword: string, host: string, port: string) {
-		throw new Error("Method not implemented.");
+	private db: any;
+	
+	connect(uname?: string, pword?: string, host?: string, port?: string) {
+		this.db = new SQLiteDb(':memory:');
+		
+		// create table
+		this.db.prepare('CREATE TABLE IF NOT EXISTS dogelon(callingFunction TEXT, reference TEXT, cacheUntil INTEGER, jsonData TEXT)').run();
 	}
 	get(key: string) {
 		throw new Error("Method not implemented.");
--- src/types/Database.ts
+++ src/types/Database.ts
@@ -1,5 +1,5 @@
 export default interface Database {
+	connect(uname: string, pword: string, host: string, port: string),
 	get(key:string ): any,
 	set(key: string, ...arg0: any): void,
-	connect(uname: string, pword: string, host: string, port: string),
 }
