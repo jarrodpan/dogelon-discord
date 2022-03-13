bcb019f6fe9d4341b7896101f36e756237235d7c
Jarrod <jarrod.pan@gmail.com>
2022-03-13 16:49:14 +1100

    unit test collection

--- src/types/Database.ts
+++ src/types/Database.ts
@@ -1,7 +1,26 @@
 export default abstract class Database {
+	/**
+	 * Connect to a database.
+	 * @param uname Username if required
+	 * @param pword Password if required
+	 * @param host Hostname if required
+	 * @param port Port number if required
+	 */
 	public abstract connect(uname?: string, pword?: string, host?: string | 'localhost', port?: string | 1433);
-	public abstract get(key: string): any;
-	public abstract set(key: string, val: any, cache?: number): any;
+	/**
+	 * Gets a cached object
+	 * @returns The cached object if found and not expired, false otherwise
+	 * @param key Cache key to query for
+	 */
+	public abstract get(key: string): object | false;
+	/**
+	 * Sets or updates the cached object at the specified key.
+	 * @returns 1 if the row is updated, false if an error occurs.
+	 * @param key Key to set or update
+	 * @param val JSON object to set
+	 * @param cache Optional. Set expiry time of the cache value.
+	 */
+	public abstract set(key: string, val: any, cache?: number): number | false;
 	
-	protected static unixTime() { return Math.ceil(Date.now() / 1000); }
+	public static unixTime() { return Math.ceil(Date.now() / 1000); }
 }
