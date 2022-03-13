afd19b0704fa021c3391fff982324b5af1af16cc
Jarrod <jarrod.pan@gmail.com>
2022-03-13 12:04:06 +1100

    getter and setter

--- src/resolvers/SQLiteDatabase.ts
+++ src/resolvers/SQLiteDatabase.ts
@@ -1,7 +1,7 @@
 import Database from "../types/Database";
 import SQLiteDb from 'better-sqlite3'; // alias of 'Database'
 
-export default class SQLiteDatabase implements Database {
+export default class SQLiteDatabase extends Database {
 	private db: any;
 	
 	connect(uname?: string, pword?: string, host?: string, port?: string) {
@@ -11,17 +11,25 @@ export default class SQLiteDatabase implements Database {
 		this.db.prepare('CREATE TABLE IF NOT EXISTS dogelon(key TEXT, jsonData TEXT, cacheUntil INTEGER)').run();
 	}
 	get(key: string) {
-		const time = Math.ceil(Date.now() / 1000);
+		const time = Database.unixTime();//Math.ceil(Date.now() / 1000);
 		const stmt = this.db.prepare("SELECT json_object(jsonData) FROM dogelon WHERE key = ? AND cacheUntil > ?");
 		try {
-			return stmt.get(key, time);
+			return stmt.get(key, time) || false;
 		} catch (e) {
 			console.error(e);
 			return false;
 		}
 	}
-	set(key: string, val: any, cache?: number): void {
-		throw new Error("Method not implemented.");
+	set(key: string, val: any, cache?: number) {
+		if (!cache) cache = Database.unixTime();
+		const stmt = this.db.prepare("INSERT OR REPLACE INTO dogelon (key, jsonData, cacheUntil) VALUES (?, ?, ?)");
+		try {
+			const info = stmt.run(key, JSON.stringify(val), cache);
+			return info.change;
+		} catch (e) {
+			console.error(e);
+			return false;
+		}
 	}
 	
 }
--- src/types/Database.ts
+++ src/types/Database.ts
@@ -2,4 +2,6 @@ export default abstract class Database {
 	public abstract connect(uname?: string, pword?: string, host?: string | 'localhost', port?: string | 1433);
 	public abstract get(key: string): any;
 	public abstract set(key: string, val: any, cache?: number): any;
+	
+	protected static unixTime() { return Math.ceil(Date.now() / 1000); }
 }
