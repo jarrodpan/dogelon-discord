bf95b9f1bd4594f7472bd59f9ddeaaa28f72c0dc
Jarrod <jarrod.pan@gmail.com>
2022-03-17 15:23:28 +1100

    interface uplift

--- src/commands/LigmaCommand.ts
+++ src/commands/LigmaCommand.ts
@@ -1,4 +1,4 @@
-import { Message } from 'discord.js';
+import { Message, TextChannel } from 'discord.js';
 import { Command, MatchOn } from '../types/Command'
 import Database from '../types/Database';
 
@@ -11,7 +11,7 @@ export default class LigmaCommand extends Command {
 	public expression = "what(?:'{0,1}| | i)s ligma\\?*"; //"what's ligma";
 	public matchOn = MatchOn.MESSAGE;
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	public execute = (message: Message, _: unknown) => {
+	public execute = (message: Message | TextChannel, _: unknown) => {
 		return "ligma balls";
 	}
 }
