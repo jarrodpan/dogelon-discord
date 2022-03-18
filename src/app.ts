a2f5548aa8c6600f611382941bd001802f4375cd
Jarrod <jarrod.pan@gmail.com>
2022-03-19 02:20:22 +1100

    bold notice

--- src/app.ts
+++ src/app.ts
@@ -163,7 +163,7 @@ const newDeploy = (channels) => {
 	const embed = new MessageEmbed()
 		.setColor("#9B59B6")
 		.setTitle(`🚀  Dogelon Update - ` + title)
-		.setDescription("*Notice*: database is not persistent so you will have to rerun `!subscribe` for all feeds. This will be fixed in a future update.")
+		.setField("Notice:","Database is not persistent so you will have to rerun `!subscribe` for all feeds. This will be fixed in a future update.")
 		.setThumbnail("https://i.imgur.com/1LIQGWa.png")
 		//.setTimestamp()
 		.setFooter({ text: `Dogelon ${v}  •  ${a}` })
