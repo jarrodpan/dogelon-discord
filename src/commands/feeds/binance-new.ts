5cb16acfc735ed0fcd30ba375d382150091250bd
Jarrod <jarrod.pan@gmail.com>
2022-04-02 01:46:34 +1100

    type definition and implementation

--- src/commands/feeds/binance-new.ts
+++ src/commands/feeds/binance-new.ts
@@ -4,8 +4,13 @@ import { Feed } from '../../types/Feed';
 export default class binanceNew implements Feed {
 	private db: Database;
 	public readonly feedName = 'binance-new';
+	public readonly updateTime: number = 600000;
 
 	constructor(db: Database) {
 		this.db = db;
+	}
+
+	public intervalFunction(): void {
+		throw new Error('Method not implemented.');
 	}
 }
--- src/types/Feed.d.ts
+++ src/types/Feed.d.ts
@@ -1,5 +1,14 @@
 export interface Feed {
-	feedName: string;
-
-	//constuctor(db: Database): void;
+	/**
+	 * The command to call the feed from discord.
+	 */
+	readonly feedName: string;
+	/**
+	 * How frequently to check for updates on feed in milliseconds
+	 */
+	readonly updateTime: number;
+	/**
+	 * Callback which defines the update checking logic for the feed.
+	 */
+	public readonly intervalFunction(): void;
 }
