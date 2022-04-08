6c90a38fbc94ae8e0be70c2d388fb495e06707ad
Jarrod <jarrod.pan@gmail.com>
2022-04-08 14:02:27 +1000

    rename

---
+++ src/dogelon/Queue.ts
@@ -0,0 +1,24 @@
+import { Channel, Message, TextChannel } from 'discord.js';
+
+type Action = {
+	message: Channel;
+	token: string;
+	callback: (
+		message: Message | TextChannel,
+		input: string
+	) => Promise<any> | any;
+};
+
+export class Queue {
+	private static queue: Action[] = [];
+
+	public static push(
+		message: Channel,
+		token: string,
+		callback: (...params: any) => any
+	) {
+		Queue.queue.push({ message, token, callback } as Action);
+	}
+
+	public static processNext = async () => {};
+}
