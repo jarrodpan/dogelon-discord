3d3a6ca1f8fa3f2c33b91bd709ca24a534febd65
Jarrod <jarrod.pan@gmail.com>
2022-04-11 01:37:07 +1000

    full pagination

--- src/commands/HelpCommand.ts
+++ src/commands/HelpCommand.ts
@@ -51,38 +51,14 @@ export default class HelpCommand extends Command {
 			page.forEach((field) => {
 				embed.addField(field.title, field.body);
 			});
-		}
-
-		embed
-			.setDescription(
-				'The not-so-stupid discord bot made for no reason. Written in Node.js and TypeScript.\n\nAvailable options for `!help`:\n\n' +
+			//embed.setDescription(`Help page for \`${option}\``);
+			embed.setTitle('🚀  Dogelon - Help page for `' + option + '`');
+		} else
+			embed.setDescription(
+				'The not-so-stupid discord bot made for no reason. Written in Node.js and TypeScript.\n\nAvailable options for `!help {option}`:\n\n' +
 					this.defaultPage
-			)
-			.addField(
-				'`${stock ticker}` (inline)',
-				'Gets current price for a stock. Cached and/or delayed depending on API response and exchange opening times. Sourced from [Yahoo Finance](https://finance.yahoo.com/).'
-			)
-			.addField(
-				'`%{ticker}(:selection)(/currency/time)` (inline)\n`%!{ticker}:{preference}` (inline)',
-				'Gets current price for a cryptocurrency. Cached and/or delayed. Examples: `%eth`, `%gala/aud`, `%btc/jpy/1y`, `%doge:1//2w`.\n\
-				Example to set preferences for multiple choice: `%!eth:1`.\n\
-				Timescale options: `1h`, `24h`, `7d`, `14d`, `30d`, `60d`, `200d`, `1y` (with various aliases).\n\
-				Sourced from [CoinGecko](https://www.coingecko.com).'
-			)
-			.addField(
-				'`/r/{subreddit}`, `r/{subreddit}` (inline)',
-				'Converts a subreddit reference to a reddit link, regardless of if it exists or not.'
-			)
-			.addField(
-				'`!subscribe {feed}`, `!s {feed}`\n`!unsubscribe {feed}`, `!uns {feed}`',
-				'Subscribe/unsubscribe a channel to a news feed. Polls once every half hour. Current options:\n - [`binance-new`](https://www.binance.com/en/support/announcement/c-48)'
-			)
-			.addField(
-				'`!binance`, `!b` (inline)',
-				'Shows the latest cryptocurrency listing news from [Binance](https://www.binance.com/en/support/announcement/c-48)'
-			)
-			.addField('`!changes`, `!c`', 'Displays latest changelog entry')
-			.addField('`!help`, `!h`', 'Displays this message');
+			);
+
 		//.setTimestamp()
 		return { embeds: [embed] };
 	};
