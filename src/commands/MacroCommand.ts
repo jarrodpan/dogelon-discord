609e95dc3ca9a615431c60e30ef96ecb93368ff9
Jarrod <jarrod.pan@gmail.com>
2022-04-11 22:45:01 +1000

    macro command

--- src/commands/MacroCommand.ts
+++ src/commands/MacroCommand.ts
@@ -5,10 +5,22 @@ import { Command, MatchOn } from '../commands/';
  * Command to register macros with the bot.
  */
 export default class MacroCommand extends Command {
-	public expression = '&(\\S*)';
+	public expression = '^&(\\S*)(=)?(\\S*)';
 	public matchOn = MatchOn.MESSAGE;
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	public execute = (message: Message | TextChannel, _: unknown) => {
+	public execute = (message: Message | TextChannel, input: string) => {
+		// set flags
+		let setDefinition;
+
+		// parse command
+		// knockout if non command gets passed in for some reason
+		if (input.slice(0, 1) !== '&') return null;
+		// split any trailling spaced off stuff, split by equals sign
+		const [macro, definition] = input.split(' ')[0].slice(1).split('=');
+
+		if (!definition || definition.length == 0) setDefinition = false;
+		else setDefinition = true;
+
 		return '';
 	};
 }
