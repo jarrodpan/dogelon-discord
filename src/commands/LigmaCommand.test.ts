46965448cfb383e7efba66fdae3fff3db79b0099
Jarrod <jarrod.pan@gmail.com>
2022-04-11 17:49:17 +1000

    move mock Command to new class

--- src/commands/LigmaCommand.test.ts
+++ src/commands/LigmaCommand.test.ts
@@ -1,19 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-jest.mock('./', () => {
-	return {
-		Command: jest.fn().mockImplementation(() => {
-			return {
-				init: () => null,
-			};
-		}),
-		MatchOn: jest.fn().mockImplementation(() => {
-			return {
-				TOKEN: 0,
-				MESSAGE: 1,
-			};
-		}),
-	};
-});
+require('./index.mock.test').default;
+
 const dummyMessage = require('discord.js').Message;
 import LigmaCommand from './LigmaCommand';
 
@@ -22,7 +9,7 @@ describe('LigmaCommand', () => {
 	jest.mock('discord.js');
 
 	it.each(['whats ligma', 'who cares'])(
-		'when input is %s output should be "ligma balls"',
+		'when input is \'%s\' output should be "ligma balls"',
 		async (input) => {
 			const res = await ligma.execute(dummyMessage, input);
 			expect(res).toEqual('ligma balls');
