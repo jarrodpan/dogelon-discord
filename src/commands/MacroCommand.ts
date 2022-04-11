5d9a0f401c675c88ff7b8ad1492e0ef5463ce050
Jarrod <jarrod.pan@gmail.com>
2022-04-12 00:31:51 +1000

    adjustments

--- src/commands/MacroCommand.ts
+++ src/commands/MacroCommand.ts
@@ -7,10 +7,10 @@ import { HelpPage } from '../types/Help';
  * Command to register macros with the bot.
  */
 export default class MacroCommand extends Command {
-	private db: Database;
-	public constructor(db: Database) {
+	private db?: Database;
+	public constructor(db?: Database) {
 		super();
-		this.db = db;
+		if (db) this.db = db;
 	}
 
 	// TODO: write help
@@ -18,36 +18,40 @@ export default class MacroCommand extends Command {
 		command: 'macro',
 		message: [
 			{
-				title: '`!subscribe {feed}`\n`!s {feed}`',
-				body: 'Subscribe a channel to a news feed.',
+				title: '`!subscribe {feed}`\n`!s {feed}`', // TODO
+				body: 'Subscribe a channel to a news feed.', // TODO
 			},
 			{
-				title: '`!unsubscribe {feed}`\n`!uns {feed}`',
-				body: 'Unsubscribe a channel from a news feed.',
+				title: '`!unsubscribe {feed}`\n`!uns {feed}`', // TODO
+				body: 'Unsubscribe a channel from a news feed.', // TODO
 			},
 		],
 	};
 
 	private macroMap = new Map<string, any>();
 
-	public expression = '^&(\\S*)(=)?(\\S*)';
+	public expression = '&(\\S*)(=)?(\\S*)';
 	public matchOn = MatchOn.MESSAGE;
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	public execute = (message: Message | TextChannel, input: string) => {
-		// set flags
+		// flags
 		let setDefinition;
 
 		// parse command
 		// knockout if non command gets passed in for some reason
 		if (input.slice(0, 1) !== '&') return null;
 		// split any trailling spaced off stuff, split by equals sign
-		const [macro, definition] = input.split(' ')[0].slice(1).split('=');
+		const [macro, ...definition] = input.split(' ')[0].slice(1).split('=>');
 
 		if (!definition || definition.length == 0) setDefinition = false;
 		else setDefinition = true;
 
-		// do database calls
+		console.debug(macro, definition);
 
-		return '';
+		// TODO: validate all commands are valid
+
+		// TODO: do database calls
+
+		return null;
 	};
 }
