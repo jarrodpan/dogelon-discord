aa574a7000456a2592558d601094af95a52b4870
Jarrod <jarrod.pan@gmail.com>
2022-04-12 11:46:24 +1000

    better grouping of tests

--- src/commands/MacroCommand.test.ts
+++ src/commands/MacroCommand.test.ts
@@ -29,17 +29,28 @@ describe('MacroCommand', () => {
 	});
 
 	describe('when macro definition is set', () => {
-		it.each([
-			'&nope=>',
-			'&nope=>=>',
-			'&nope=> ',
-			'&nope=> =>    ',
-			'&nope=>whats ligma=>fff',
-		])("should return null on invalid definition '%s'", async (input) => {
-			const res = await macro.execute(dummyMessage, input);
-			expect(res).toBeNull();
+		describe('when definition is invalid', () => {
+			it.each([
+				'&nope=>',
+				'&nope=>=>',
+				'&nope=> ',
+				'&nope=> =>    ',
+				'&nope=>whats ligma=>fff',
+			])(
+				"should return null on invalid definition '%s'",
+				async (input) => {
+					const res = await macro.execute(dummyMessage, input);
+					expect(res).toBeNull();
+				}
+			);
 		});
-		it.todo('should return confirmation embed on valid definition');
-		it.todo('should call parsing callback on defined macro');
+
+		describe('when definition is valid', () => {
+			it.todo('should return confirmation embed on valid definition');
+		});
+
+		describe('when macro has been defined', () => {
+			it.todo('should call parsing callback on defined macro');
+		});
 	});
 });
