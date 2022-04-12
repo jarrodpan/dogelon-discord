f73ee25f380fbf2ddeb5cb6d0d7f977628cc6972
Jarrod <jarrod.pan@gmail.com>
2022-04-12 11:42:37 +1000

    mock has regex match map

--- src/commands/__mocks__/index.ts
+++ src/commands/__mocks__/index.ts
@@ -1,11 +1,15 @@
-export const Command = jest.fn().mockImplementation(() => {
+export const Command = jest.fn(() => {
 	return {
 		constructor: () => null,
 		init: () => null,
+		matchOn: new Map([
+			[0, /^(?<DummyTokenCommand>[!$%](\/S*))$/],
+			[1, /^(?<DummyMessageCommand>[!$%](\/S*))$/],
+		]),
 	};
 });
 
-export const MatchOn = jest.fn().mockImplementation(() => {
+export const MatchOn = jest.fn(() => {
 	return {
 		TOKEN: 0,
 		MESSAGE: 1,
