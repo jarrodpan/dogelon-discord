f8788dbb7ea2050915d121783e451bdbcd02e703
Jarrod <jarrod.pan@gmail.com>
2022-04-14 00:23:23 +1000

    more mocking (broken)

--- src/commands/__mocks__/index.ts
+++ src/commands/__mocks__/index.ts
@@ -1,8 +1,20 @@
+import { Message, TextChannel } from 'discord.js';
+
 enum MatchOn {
 	TOKEN,
 	MESSAGE,
 }
 
+class DummyTokenCommand extends Command {
+	constructor() {
+		super();
+	}
+}
+class DummyMessageCommand extends Command {
+	constructor() {
+		super();
+	}
+}
 class Command {
 	constructor() {
 		return;
@@ -10,11 +22,23 @@ class Command {
 	public init = () => {
 		return;
 	};
+	public execute!: (
+		message: Message | TextChannel,
+		input: any
+	) => Promise<any> | any;
+
 	public static matchOn = new Map([
 		[MatchOn.TOKEN, /^(?<DummyTokenCommand>[$%](\S*))$/gm],
 		[MatchOn.MESSAGE, /^(?<DummyMessageCommand>!(\S*))$/gm],
 	]);
-}
-// for static member
 
-export { Command, MatchOn };
+	static commandMap: Map<any, any> = new Map([
+		['DummyTokenCommand', new DummyTokenCommand()],
+		['DummyMessageCommand', new DummyMessageCommand()],
+	]);
+
+	public static getExecuteFromCommandName = (command: string) =>
+		Command.commandMap.get(command).execute;
+}
+
+export { Command, MatchOn, DummyMessageCommand, DummyTokenCommand };
